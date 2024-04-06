ab26ead54b73fed29c59416a658d9a1e1e47ec8d
ThiagoVenturini <thethiagoventurini@gmail.com>
2024-04-05 21:03:09 -0300

    teste

---
+++ src/atividades/Atividade.java
@@ -0,0 +1,11 @@
+package atividades;
+
+public class Atividade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Thiago");
+		System.out.println("Teste");
+	}
+
+}
