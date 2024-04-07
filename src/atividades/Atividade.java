3000ad81401fe077ce298c6181aa799454e600ec
ThiagoVenturini <thethiagoventurini@gmail.com>
2024-04-07 13:58:00 -0300

    Atividade 01: realizando a atividade 01, usando metodos para pegar 'Dates', localdatetime', usando array para colocar meses e dias por extenso

--- src/atividades/Atividade.java
+++ src/atividades/Atividade.java
@@ -1,10 +1,26 @@
 package atividades;
+import java.time.LocalDateTime;
 
 public class Atividade {
+    public static void main(String[] args) {
+        mostrarDataPorExtenso();
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Thiago");
-	}
+    public static void mostrarDataPorExtenso() {
+        LocalDateTime dataAtual = LocalDateTime.now();
+        int diaSemana = dataAtual.getDayOfWeek().getValue();
+        int dia = dataAtual.getDayOfMonth();
+        int mes = dataAtual.getMonthValue();
+        int ano = dataAtual.getYear();
+        int horas = dataAtual.getHour();
+        int minutos = dataAtual.getMinute();
 
+        String[] diasSemana = {"", "domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"};
+        String[] meses = {"", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+
+        String diaSemanaExt = diasSemana[diaSemana];
+        String mesExt = meses[mes];
+
+        System.out.println("Hoje é " + diaSemanaExt + ", dia " + dia + " de " + mesExt + " de " + ano + " e agora são " + horas + " horas e " + minutos + " minutos.");
+    }
 }
